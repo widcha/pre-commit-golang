0bdd999b0bdb10ca9206f658be3f2d300a2b3897
Wido Wicaksono <widowicak@gmail.com>
2022-07-22 11:50:49 +0700

    Update run-go-vet.sh

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 for DIR in $(echo "$@" | xargs -n1 dirname | sort -u); do
-	go vet ./"$DIR" + "/packages/services/farm"
+# 	go vet ./"$DIR" + "/packages/services/farm"
 	echo "Halo"
 done
